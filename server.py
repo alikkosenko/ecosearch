c0cd9b330704a27a489a4c4becc9aa3d67c9da6e
alikkosenko <alikkosenko31@gmail.com>
2025-08-22 02:26:35 +0300

    Version 1

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+import table_reader as tr
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def search_page():
+    query = request.args.get("q", "")
+    results = None
+    if query:
+        results = tr.search(query)
+    return render_template("index.html", query=query, results=results)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ table_reader.py
@@ -0,0 +1,35 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+def search(model:str = None):
+    # Определяем область доступа
+    scope = ["https://spreadsheets.google.com/feeds",
+             "https://www.googleapis.com/auth/drive"]
+
+    # Загружаем ключ сервисного аккаунта
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    client = gspread.authorize(creds)
+
+    # Открываем таблицу по названию
+    sheet = client.open("Transit_test").sheet1
+
+    # Читаем все строки
+    rows = sheet.get_all_values()
+
+
+    spreadsheet=client.open_by_key("1SAWJrKfZEcGM1HGBWfIHg5pdEh9aaQfyjmGjXwKIn8U")
+    sheet = spreadsheet.worksheet('Транзит')
+
+    print(sheet.acell('E9').address)
+
+    print(f"Searching {model}...")
+    model_list = list()
+    for i, row in enumerate(rows, start=1):
+        if model.lower() in row[1].lower():
+            link = "https://docs.google.com/spreadsheets/d/1SAWJrKfZEcGM1HGBWfIHg5pdEh9aaQfyjmGjXwKIn8U/edit?"\
+                   "gid=1068582570#gid=1068582570&range=B{}".format(i)
+            row.insert(0, link)
+            model_list.append(row)
+
+    return model_list
+
