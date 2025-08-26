83db8dad7b400d3f705fd9ba71e5d49fda2d9ea0
alikkosenko <alikkosenko31@gmail.com>
2025-08-26 21:22:10 +0300

    Table change troubleshooting

--- table_reader.py
+++ table_reader.py
@@ -5,8 +5,8 @@ from datetime import datetime
 
 def parse_date(x):
     try:
-        if x[18]:
-            return datetime.strptime(x[18], "%d.%m.%Y")
+        if x[3]:
+            return datetime.strptime(x[3], "%d.%m.%Y")
         else:
             return datetime.max  # пустые даты будут в конце
     except ValueError:
@@ -24,8 +24,8 @@ def search(request: str = None) -> list[Any]:
     model_list = list()
     # retrieving appropriate rows
     for i, row in enumerate(rows, start=1):
-        print(row[23])
-        if request.lower() in row[23].lower() and row[0] != "":
+        print(row[22])
+        if request.lower() in row[22].lower() and row[0] != "":
             link = "https://docs.google.com/spreadsheets/d/1PNjWz8LTArpcRovsEUTjd543oEjRHeh7D_N8o4EhIvA/edit?"\
                    "gid=1640256538#gid=1640256538&range=B{}".format(i)
             row.insert(0, link)
@@ -46,13 +46,13 @@ def search(request: str = None) -> list[Any]:
     model_list = ([i for i in model_list if "в наявност" in i[20].lower() or "в наявност" in i[21].lower()]
                   + [i for i in model_list if "в наявност" not in i[20].lower() or "в наявност" not in i[21].lower()])
 
-    model_list = [i for i in model_list if not i[22]] + [i for i in model_list if i[22]]
+    model_list = [i for i in model_list if not i[4]] + [i for i in model_list if i[4]]
 
     for i in model_list:
-        if not i[22]:
-            i[22] = "-"
-        if not i[18]:
-            i[18] = "-"
+        if not i[20]:
+            i[20] = "-"
+        if not i[21]:
+            i[21] = "-"
 
 
     return model_list
