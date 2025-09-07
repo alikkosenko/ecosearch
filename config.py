4dc665f66c4cb20c1af3778e84940ae34ebe650e
alikkosenko <alikkosenko31@gmail.com>
2025-09-07 21:36:49 +0300

    Forget about config

---
+++ config.py
@@ -0,0 +1,21 @@
+DRAWINGS = "drawing1.xml"
+NAMESPACES = {
+        "xdr": "http://schemas.openxmlformats.org/drawingml/2006/spreadsheetDrawing",
+        "a": "http://schemas.openxmlformats.org/drawingml/2006/main"
+}
+
+SCOPE = ["https://spreadsheets.google.com/feeds",
+         "https://www.googleapis.com/auth/drive"]
+SHEET = "Транзит Авто**"
+DATA = "data.pkl"
+CREDS = "credentials.json"
+
+ARRIVAL_DATE = 3
+NOMENCLATURE_NAME = 22
+IMG_NAME = 0
+VIN = 0
+STATUS = 20
+STORAGE = 21
+RESERVE = 4
+TABLE_LINK = "https://docs.google.com/spreadsheets/d/1PNjWz8LTArpcRovsEUTjd543oEjRHeh7D_N8o4EhIvA/edit?"\
+                   "gid=1640256538#gid=1640256538&range=B{}"
